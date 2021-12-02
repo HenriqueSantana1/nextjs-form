ee26162fe0db4dd12a620f9f5fe44d5860b77cb8
Henrique Vuolo <henriquevuolo@gmail.com>
2021-12-02 19:57:40 -0300

    feat: cria template Layout e aplica nas paginas

---
+++ src/pages/signup.tsx
@@ -0,0 +1,9 @@
+import Layout from '@/components/templates/Layout'
+
+export default function SignUpPage() {
+  return (
+    <Layout title="Cadastre-se">
+      <h1>Hello, cadastre-se</h1>
+    </Layout>
+  )
+}
